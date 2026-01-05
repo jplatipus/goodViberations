7550e3b50e8d98b4de46f66729b91a2b70e19052
Jacques Leplat <jacques@j3ltd.com>
2026-01-05 08:50:48 +0000

    Material design v 3, updated top bar to not overlap activity widgets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
+    implementation("com.google.android.material:material:1.11.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
